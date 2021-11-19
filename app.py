3cb982bbdb8643d836d352f74616a9daa6df0250
Nicholas R <32598910+NicholasRosato@users.noreply.github.com>
2021-11-18 20:50:07 -0500

    return json format

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # EPICS magnifier flask server
 # last updated 11/17/2021
 
-from flask import Flask
+from flask import Flask, jsonify
 import os
 import cv2
 import base64
@@ -22,9 +22,9 @@ def get_image():
 		print(app.config["CLIENT_IMAGES"] + image_name)
 		image_file = open(app.config["CLIENT_IMAGES"] + image_name, "rb")
 		encoded_string = base64.b64encode(image_file.read())
-		return encoded_string
+		return jsonify(message=encoded_string)
 	except FileNotFoundError:
-		return "FILE_ERROR"
+		return jsonify(message="FILE_ERROR")
 
 @app.route("/capture_image")
 def capture_image():
@@ -34,27 +34,27 @@ def capture_image():
 
 	if not ret:
 		print("Failed to capture frame")
-		return "CAPTURE_ERROR"
+		return jsonify(message="CAPTURE_ERROR")
 
 	img_name = "magnifier_image.png"
 	cv2.imwrite(img_name, frame)
 	print("Image written as {}".format(img_name))
 
 	cam.release() 
-	return "CAPTURE_SUCCESS"
+	return jsonify(message="CAPTURE_SUCCESS")
 
 @app.route("/start_motion")
 def start_motion():
 	print("Starting motion video server...")
 	os.system("sudo service motion start")
-	return "MOTION_START"
+	return jsonify(message="MOTION_START")
 
 @app.route("/stop_motion")
 def stop_motion():
 	print("Stopping motion video server...")
 	os.system("sudo service motion stop")
-	return "MOTION_STOP"
+	return jsonify(message="MOTION_STOP")
 
 @app.route("/")
 def index():
-	return "INDEX"
+	return jsonify(message="INDEX")
