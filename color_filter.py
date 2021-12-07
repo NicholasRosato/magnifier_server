7dc74df30f0b26d59d31a48ade3a8f124637c282
Nicholas R <32598910+NicholasRosato@users.noreply.github.com>
2021-12-07 16:33:10 -0500

    Update color_filter.py

--- color_filter.py
+++ color_filter.py
@@ -6,16 +6,19 @@
 import cv2
 import numpy as np
 
-def filter(frame, option):
-    # convert from BLUE GREEN RED (RGB) color space to HSV color space
-    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+def filter(frame, option):    
     print ("Filtering color for frame...")
-    
-    result = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+       
+    # convert image to grayscale
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # filter the color with binary threashold to make it black and white
+    (thresh, bw_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
 
     # THIS CODE IS FOR CONVERTING TO A SPECIFIC COLOR, FOR NOW WE JUST CONVERT TO GREYSCALE
+    # This uses a mask operation to do so, leaving it in here for someone who wants to experiment with it
     # threshold for color
+    # convert from BLUE GREEN RED (RGB) color space to HSV color space
+    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #lower = np.array([60, 35, 140])
     #upper = np.array([180, 255, 255])
     
@@ -28,4 +31,4 @@ def filter(frame, option):
     # remove all non colored regions by ANDing with the mask
     #result = cv2.bitwise_and(frame, frame, mask = mask)
 
-    return result
+    return bw_image
